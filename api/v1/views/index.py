1a44031df54f85226f9911c8129226e57d8cdcfe
BattousaiW <Brandonwee08@gmail.com>
2024-04-24 17:00:01 +0200

    RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """flask route that returns json status"""
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -12,3 +13,21 @@ def status():
     response = {"status": "OK"}
     resp = jsonify(response)
     return resp
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of object routes
+    """
+    obj = {
+        "amenities": storage.count("Amenities"),
+        "cities": storage.count("Cities"),
+        "places": storage.count("Places"),
+        "reviews": storage.count("Reviews"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+
+    response = jsonify(obj)
+    return response
