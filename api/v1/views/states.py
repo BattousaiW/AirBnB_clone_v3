5a9d7196bf356eecaeb997668b8768fbc4ba0338
BattousaiW <Brandonwee08@gmail.com>
2024-04-25 13:30:23 +0200

    web framework

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""""View for state objects that handle RESTful API
-actions
-"""
+""""View for state objects that handle RESTful API actions"""
 from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
 from models.state import State
