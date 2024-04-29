494dd1de188b202af07e6f9f8f5e824ff735caf2
BattousaiW <Brandonwee08@gmail.com>
2024-04-29 11:16:26 +0200

    Restful API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-View for handling default RESTFUL API actions
-"""
-from flask import jsonify, request, abort
-from api.v1.views import app_views, storage
-from models.state import State
+"""flask route that returns json status"""
+from api.v1.views import app_views
+from flask import jsonify, request
+from models import app_views, storage
